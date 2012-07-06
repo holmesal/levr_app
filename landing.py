0c7e6affecf5643d342a490bae04e4d0668714e9
Alonso Holmes <holmesal@bu.edu>
2012-07-05 20:08:53 -0400

    Added static landing page for root url
    Using a custom app (landing.py) and jinja to render the content isn't
    the best way to do this, but it'll do for now :-)
    
    Also, I added the directories css, img, and js as static directories.
    they're reachable by any html file now via script includes or image
    src, etc

---
+++ landing.py
@@ -0,0 +1,19 @@
+#there's probably a more elegant way to serve up a static landing page that doesn't depend on loading jinja2 and then just redirecting instantly. This'll do for now.
+
+import os
+import webapp2
+import jinja2
+
+class landing(webapp2.RequestHandler):
+	def get(self):
+		#launch the jinja environment
+		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+		
+		#not sure if this can be empty, spoofing till i can test later
+		template_values = {}
+		
+		#serve up the landing page html
+		template = jinja_environment.get_template('landing.html')
+		self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', landing)],debug=True)

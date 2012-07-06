64688f5e709059d9f67ff5ab4e7c5695f23942c2
Alonso Holmes <holmesal@bu.edu>
2012-07-06 02:21:55 -0400

    Added login-dependant HTML. Going to bed.
    loginCheck has been extended a bit to send back an object called
    headerData. It now has two modes:
    If strict=True, redirects to login (no headerData)
    If strict=False, returns headerData without redirecting. This is useful
    on pages that can be accessed by both logged-in and non-logged-in
    merchants, like the landing page (/merchants) and the login page (will
    add dynamic headers to this later)

--- levr_utils.py
+++ levr_utils.py
@@ -5,13 +5,23 @@ import jinja2
 
 from gaesessions import get_current_session
 
-def loginCheck(self):
+def loginCheck(self,strict):
+	'''This is a general-purpose login checking function
+	   in "strict" mode (strict=True), this script will bounce to the login page if not logged in
+	   if strict=False, headers will be returned that indicate the user isn't logged in, but no bouncing'''
 	session = get_current_session()
 	logging.info(session)
 	if session.has_key('loggedIn') == False or session['loggedIn'] == False:
-		#not logged in, bounce to login page
-		logging.info('Not logged in. . .Bouncing!')
-		self.redirect('/login')
+		if strict == True:
+			#not logged in, bounce to login page
+			logging.info('Not logged in. . .Bouncing!')
+			self.redirect('/login')
+		else:
+			logging.info('Not logged in. . .Sending back headerData')
+			headerData = {
+				'loggedIn'	: False
+			}
+			return headerData
 	elif session.has_key('loggedIn') == True or session['loggedIn'] == True:
 		#logged in, grab the useful bits
 		headerData = {
--- merchants.py
+++ merchants.py
@@ -8,40 +8,62 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
     
 class merchantsLanding(webapp2.RequestHandler):
 	def get(self):
-		template = jinja_environment.get_template('templates/merchantsLanding.html')
-		self.response.out.write(template.render())
+		#Get headers if user isn't logged in
+		headerData = levr_utils.loginCheck(self,False)
+		logging.info(headerData)
+		template_values = {
+			'headerData' : headerData,
+			'title' : 'Welcome'
+		}
+		
+		template = jinja_environment.get_template('templates/header.html')
+		self.response.out.write(template.render(template_values))
 
 class manage(webapp2.RequestHandler):
 	def get(self):
 		#Bounce if user is not logged in
-		headerData = levr_utils.loginCheck(self)
-		logging.info(headerData)
+		headerData = levr_utils.loginCheck(self,True)
+		
+		template_values = {
+			'headerData' : headerData,
+			'title' : 'Manage'
+		}
+
+		template = jinja_environment.get_template('templates/header.html')
+		self.response.out.write(template.render(template_values))
 
 class new_deal(webapp2.RequestHandler):
 	def get(self):
 		#Bounce if user is not logged in
-		levr_utils.loginCheck(self)
+		levr_utils.loginCheck(self,True)
 		
 		template_values = {
-			'name' : 'Alonso'
+			'title' : 'New Deal'
 		}
 		
-		template = jinja_environment.get_template('templates/test.html')
+		template = jinja_environment.get_template('templates/header.html')
 		self.response.out.write(template.render(template_values))
-		template = jinja_environment.get_template('templates/merchantsLanding.html')
-		self.response.out.write(template.render())
 		
 class edit_deal(webapp2.RequestHandler):
 	def get(self):
 		#Bounce if user is not logged in
-		levr_utils.loginCheck(self)
+		levr_utils.loginCheck(self,True)
 		
-		#Still here? Good! 
-		
+		template_values = {
+			'title' : 'Edit Deal'
+		}
+		template = jinja_environment.get_template('templates/header.html')
+		self.response.out.write(template.render(template_values))
 class account(webapp2.RequestHandler):
 	def get(self):
 		#Bounce if user is not logged in
-		levr_utils.loginCheck(self)
-		print "This is a request for account"
+		levr_utils.loginCheck(self,True)
+		
+		template_values = {
+			'title' : 'Account'
+		}
+		
+		template = jinja_environment.get_template('templates/header.html')
+		self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/merchants', merchantsLanding), ('/merchants/manage',manage), ('/merchants/deal/new', new_deal), ('/merchants/deal/edit', edit_deal), ('/merchants/account',account)],debug=True)

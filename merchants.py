6d255fa8af41da4e0bd53cd18d6f8786cd684a0e
pnw <patrick@getlevr.com>
2012-07-09 15:54:07 -0400

    added manage table. template needs work

--- merchants.py
+++ merchants.py
@@ -3,9 +3,10 @@ import jinja2
 import os
 import levr_utils
 import logging
+import levr_classes as levr
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-    
+
 class merchantsLanding(webapp2.RequestHandler):
 	def get(self):
 		#Get headers if user isn't logged in
@@ -69,5 +70,44 @@ class account(webapp2.RequestHandler):
 		
 		template = jinja_environment.get_template('templates/header.html')
 		self.response.out.write(template.render(template_values))
-
+class manage(webapp2.RequestHandler):
+	def get(self):
+		##get logged in state
+		headerData	= levr_utils.loginCheck(self,True)
+		logging.info("=====================================")
+		logging.debug(headerData)
+		businessID	= headerData['businessID']
+		##get deal values from database for the logged in merchant
+		q	 		= levr.Deal.gql("WHERE businessID=:1",businessID)
+		##set template values in form vals = {deal1 = {val1,val2},deal2 = {val1,val2}}
+		deals = []
+		for d in q:
+		##for each deal:
+			##grab the primary categories
+			q2 = levr.Category.gql("WHERE dealID=:1",d.dealID)
+			prim_stack = []
+			for p in q2:
+				prim_stack.append(p.primary_cat)
+			##create deal dict that has all the info needed to populate table
+			deals.append({
+				'dealID'		: d.dealID,
+				'secondary_name': d.secondary_name,
+				'description'	: d.description,
+				'deal_type'		: d.deal_type,
+				'deal_value'	: d.deal_value,
+				'count_max'		: d.count_max,
+				'count_redeemed': d.count_redeemed,
+				'img_path'		: d.img_path,
+				'primary_cats'	: prim_stack
+				})
+		##Create template dictionary
+		template_values = {
+			'deals'	: deals,
+			'headerData'	: headerData,
+			'title'			: 'Manage'
+			}
+		##create view and send values to template
+		template = jinja_environment.get_template('templates/manage.html')
+		self.response.out.write(template.render(template_values))
+		#self.response.out.write(template_values)
 app = webapp2.WSGIApplication([('/merchants', merchantsLanding), ('/merchants/manage',manage), ('/merchants/deal/new', new_deal), ('/merchants/deal/edit', edit_deal), ('/merchants/account',account)],debug=True)

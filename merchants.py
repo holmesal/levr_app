7a7be1dc2ce71e2dd3b8a8c95f51aab5b33820d9
pnw <patrick@getlevr.com>
2012-07-14 17:53:27 -0400

    changed new deal creation to reflect new database tree structure

--- merchants.py
+++ merchants.py
@@ -62,40 +62,14 @@ class new_deal(webapp2.RequestHandler):
 		formdata = self.request.body
 		logging.info(formdata)
 		
-		
-		#create a new deal object (but don't store yet)
-		#this will be the same for both new and existing users
-		deal = levr.Deal()
-		#map request parameters to deal object parameters
-		
-		deal.name_type			 	= self.request.get('nameType')
-		deal.discount_type			= self.request.get('discountType')
-		#(deal_rating)
-		deal.deal_origin			= 'internal'
-		deal.count_end				= int(self.request.get('endValue'))
-		deal.city					= self.request.get('dealCity')
-		
-		logging.info(deal.discount_type)
-		if deal.discount_type == "free":
-			pass
-		else:
-			deal.discount_value		= float(self.request.get('dealValue'))
-		
-		if deal.name_type == "itemName":
-			deal.secondary_name = self.request.get('specificName')
-			deal.description = self.request.get('specificDescription')
-		elif deal.name_type == "category":
-			deal.secondary_name = self.request.get('categoryTag')
-			deal.description = self.request.get('categoryDescription')
-		
-		
+		######### GRAB CURRENT MERCHANT OR CREATE NEW ##########
 		#get session, check loginstate
-		#hello ethan
 		session = get_current_session()
 		
+		#Check login state
 		if session.has_key('loggedIn') == False or session['loggedIn'] == False:
 			#not logged in, create new business
-			business = levr.Business()
+			business = levr.Business() #no parent, business is root entity
 			#map request parameters to business object
 			business.email 			= self.request.get('email')
 			business.pw				= self.request.get('password')
@@ -110,8 +84,8 @@ class new_deal(webapp2.RequestHandler):
 			
 			
 			#check that email is available
-			q = business.gql("WHERE email = :email",email=email)
-			#if exists, write error page and exit
+			q = business.gql("WHERE email = :1",business.email)
+			#if email exists, write error page and exit
 			for result in q:
 				logging.info('duplicate email in use')
 				template = jinja_environment.get_template('templates/error.html')
@@ -122,15 +96,7 @@ class new_deal(webapp2.RequestHandler):
 			#create business
 			business.put()
 			
-			#add business properties to deal
-			#add businessID, businessName to deal
-			deal.businessID = business.key().__str__()
-			deal.business_name = business.business_name
-			
-			#insert deal into DB
-			deal.put()
-			
-			#login
+			#log in the newly created business account
 			session['businessID'] = business.key()
 			session['contact_owner'] = business.contact_owner
 			session['loggedIn'] = True
@@ -138,19 +104,42 @@ class new_deal(webapp2.RequestHandler):
 			
 		elif session.has_key('loggedIn') == True and session['loggedIn'] == True:
 			#logged in, grab current business info
+			business = session['businessID']
+		#!! end of if. the rest of the code operates independent of login state
+		
+		####### BUSINESS HAS EITHER BEEN CREATED OR EXISTING BUSINESS KEY IS GRABBED
+		#create new deal that is child of the business		
+		deal = levr.Deal(parent=business)
+		
+		#Grab form data and apply to new deal object
+		#deal.name_type		= self.request.get('nameType')
+		### overwrite values until form has changed
+		deal.name_type = 'itemName'
+		deal.discount_type	= self.request.get('discountType')
+		deal.deal_origin	= 'internal'
+		deal.count_end		= int(self.request.get('endValue'))
+		deal.city			= self.request.get('dealCity')
+		if deal.discount_type == "free":
 			pass
+		else:
+			deal.discount_value		= float(self.request.get('dealValue'))
 		
-
-		#get dealID
-		dealID = deal.key().__str__()
+		if deal.name_type == "itemName":
+			deal.secondary_name = self.request.get('specificName')
+			deal.description = self.request.get('specificDescription')
+		elif deal.name_type == "category":
+			deal.secondary_name = self.request.get('categoryTag')
+			deal.description = self.request.get('categoryDescription')
 		
-		#put category mappings into db
+		#put deal in database and key is created
+		deal.put()
+		############ DEAL HAS BEEN CREATED, CREATE PRIMARY CATS AS CHILDREN OF DEAL
 		#for now, put request tags into list
+		#prim_stack will turn into a list of variable length
 		prim_stack = [self.request.get('dealTag1'),self.request.get('dealTag2'),self.request.get('dealTag3')]
-		#build and store
+		#create each primary_cat as a child of the deal that was just created
 		for tag in prim_stack:
-			category = levr.Category()
-			category.dealID = dealID
+			category = levr.Category(parent=deal)
 			category.primary_cat = tag
 			category.put()
 		
@@ -161,14 +150,15 @@ class edit_deal(webapp2.RequestHandler):
 	def get(self):
 		#Bounce if user is not logged in
 		headerData = levr_utils.loginCheck(self,True)
-		
+		#set tamplate variables
 		template_values = {
 			'headerData' : headerData,
 			'title' : 'Edit Deal'
 		}
-		
+		#create template, render, and push to browser
 		template = jinja_environment.get_template('templates/header.html')
 		self.response.out.write(template.render(template_values))
+		
 class account(webapp2.RequestHandler):
 	def get(self):
 		#Bounce if user is not logged in
@@ -263,4 +253,9 @@ class manage(webapp2.RequestHandler):
 		##create view and send values to template
 		template = jinja_environment.get_template('templates/manage.html')
 		self.response.out.write(template.render(template_values))
-app = webapp2.WSGIApplication([('/merchants', merchantsLanding), ('/merchants/manage',manage), ('/merchants/deal/new', new_deal), ('/merchants/deal/edit', edit_deal), ('/merchants/account',account)],debug=True)
+app = webapp2.WSGIApplication([('/merchants', merchantsLanding),
+								('/merchants/manage',manage),
+								('/merchants/deal/new', new_deal),
+								('/merchants/deal/edit', edit_deal),
+								('/merchants/account',account)],
+								debug=True)

c09a24f34c65ed545ea564caecbf9d48af716e63
pnw <patrick@getlevr.com>
2012-07-19 15:32:53 -0400

    error handling on img and upload deal

--- phone.py
+++ phone.py
@@ -361,70 +361,75 @@ class phone(webapp2.RequestHandler):
 		
 class uploadDeal(webapp2.RequestHandler):
 	def post(self):
-		logging.info(self.request.headers)
-		logging.info('Body is next!')
-#		logging.info(self.request.body)
+		toEcho = {"success":False}
+		try:
+			logging.info(self.request.headers)
+			logging.info('Body is next!')
+	#		logging.info(self.request.body)
 		
-		#create alias for self.request.get
-		inputs			= self.request.get
-		#grab existing business
-		business_name	= inputs('businessName')
-		geo_point		= inputs('geoPoint')
-		logging.info(geo_point)
-		geo_point		= levr.geo_converter(geo_point)
-		logging.info(geo_point)
-		business = levr.Business.gql("WHERE business_name=:1 and geo_point=:2", business_name, geo_point).get()
-		#if a business doesn't exist in db, then create a new one
-		if not business:
-			business = levr.Business()
+			#create alias for self.request.get
+			inputs			= self.request.get
+			#grab existing business
+			business_name	= inputs('businessName')
+			geo_point		= inputs('geoPoint')
+			logging.info(geo_point)
+			geo_point		= levr.geo_converter(geo_point)
+			logging.info(geo_point)
+			business = levr.Business.gql("WHERE business_name=:1 and geo_point=:2", business_name, geo_point).get()
+			#if a business doesn't exist in db, then create a new one
+			if not business:
+				business = levr.Business()
 		
-		#populate entity
+			#populate entity
 		
-		business.geo_point		= geo_point
-		business.business_name	= business_name
-		business.address_line1	= inputs('addressLine1')
-		business.city			= inputs('city')
-		business.state			= inputs('state')
-		business.zip_code		= inputs('zip')
-#		put business in db
-		business.put()
-		logging.info(business)
-		
-		
-		uid = inputs('uid')
-		logging.info(uid)
-		#create new deal object as child of the uploader Customer
-		deal 				= levr.CustomerDeal(parent=db.Key(uid))
-		deal.img			= inputs('img')			#D
-		deal.businessID		= business.key().__str__()
-		deal.business_name	= business_name
-		deal.secondary_name	= inputs('name') #### check name!!!
-		deal.deal_status	= 'pending'
-		deal.geo_point		= geo_point
-		deal.description	= inputs('description')
-		#set expiration date to one week from now
-		#only need date, not time for this
-		deal.date_end		= datetime.now() + timedelta(days=7)
-#		date_uploaded		= automatic
-		
-		#put in DB
-		deal.put()
-		
-		#return deal id and shareURL
-		dealID = deal.key().__str__()
-		toEcho = {"success":True,"dealID":dealID,"shareURL":'http://getlevr.com/share/deal?id='+dealID}
+			business.geo_point		= geo_point
+			business.business_name	= business_name
+			business.address_line1	= inputs('addressLine1')
+			business.city			= inputs('city')
+			business.state			= inputs('state')
+			business.zip_code		= inputs('zip')
+	#		put business in db
+			business.put()
+			logging.info(business)
 		
 		
-		#send mail to the admins to notify of new pending deal
-		mail.send_mail(sender="Pending Deal <feedback@getlevr.com>",
-						to="Patrick Walsh <patrick@getlevr.com>",
-						subject="New pending deal",
-						body="""
-						Another dealdebeast has been caught by one of your
-						faithful ninjas! Heed your call to arms and approve the 
-						dealdebeast before it gets away!
-						""").send()
-		self.response.out.write(json.dumps(toEcho))
+			uid = inputs('uid')
+			logging.info(uid)
+			#create new deal object as child of the uploader Customer
+			deal 				= levr.CustomerDeal(parent=db.Key(uid))
+			deal.img			= inputs('img')			#D
+			deal.businessID		= business.key().__str__()
+			deal.business_name	= business_name
+			deal.secondary_name	= inputs('name') #### check name!!!
+			deal.deal_status	= 'pending'
+			deal.geo_point		= geo_point
+			deal.description	= inputs('description')
+			#set expiration date to one week from now
+			#only need date, not time for this
+			deal.date_end		= datetime.now() + timedelta(days=7)
+	#		date_uploaded		= automatic
+		
+			#put in DB
+			deal.put()
+		
+			#return deal id and shareURL
+			dealID = deal.key().__str__()
+			toEcho = {"success":True,"dealID":dealID,"shareURL":'http://getlevr.com/share/deal?id='+dealID}
+		
+		
+			#send mail to the admins to notify of new pending deal
+			mail.send_mail(sender="Pending Deal <feedback@getlevr.com>",
+							to="Patrick Walsh <patrick@getlevr.com>",
+							subject="New pending deal",
+							body="""
+							Another dealdebeast has been caught by one of your
+							faithful ninjas! Heed your call to arms and approve the 
+							dealdebeast before it gets away!
+							""").send()
+		except:
+			levr.log_error(self.request.body)
+		finally:
+			self.response.out.write(json.dumps(toEcho))
 		
 class phone_log(webapp2.RequestHandler):
 	def post(self):
@@ -439,71 +444,72 @@ class img(webapp2.RequestHandler):
 			size = self.request.get('size')
 			logging.info(dealID)
 			logging.info(size)
-		except:
-			logging.error('could not parse dealID or size. . . you passed:'+self.request.body)
-			sys.exit()
-		self.response.headers['Content-Type'] = 'image/jpeg'
-		#grab deal
-		deal = db.get(dealID)
-		#convert deal img to PIL object
-		img = images.Image(deal.img)
-		logging.info(img)
+			self.response.headers['Content-Type'] = 'image/jpeg'
+			#grab deal
+			deal = db.get(dealID)
+			#convert deal img to PIL object
+			img = images.Image(deal.img)
+			logging.info(img)
 		
-		#calculate height of output
+			#calculate height of output
 
-		img_width		= img.width
-		img_height		= img.height
+			img_width		= img.width
+			img_height		= img.height
 		
-		#define output parameters
-		if size == 'dealDetail':
-			#view for top of deal screen
-			aspect_ratio 	= 3. 	#width/height
-			output_width 	= 640.	#arbitrary standard
+			#define output parameters
+			if size == 'dealDetail':
+				#view for top of deal screen
+				aspect_ratio 	= 3. 	#width/height
+				output_width 	= 640.	#arbitrary standard
 		
 		
-		elif size == 'list':
-			#view for in deal or favorites list
-			aspect_ratio	= 1.	#width/height
-			output_width	= 200.	#arbitrary standard
-		elif size == 'fullSize':
-			aspect_ratio	= float(img_width)/float(img_height)
-			output_width	= float(img_width)
-#			self.response.out.write(deal.img)
+			elif size == 'list':
+				#view for in deal or favorites list
+				aspect_ratio	= 1.	#width/height
+				output_width	= 200.	#arbitrary standard
+			elif size == 'fullSize':
+				aspect_ratio	= float(img_width)/float(img_height)
+				output_width	= float(img_width)
+	#			self.response.out.write(deal.img)
 		
-		else:
-			##set this to some default for production
-			logging.error("invalid size parameter. input: "+self.request.body)
-			sys.exit()
+			else:
+				##set this to some default for production
+				logging.error("invalid size parameter. input: "+self.request.body)
+				sys.exit()
 		
-		##get crop dimensions
-		if img_width > img_height*aspect_ratio:
-			#width must be cropped
-			w_crop_unscaled = (img_width-img_height*aspect_ratio)/2
-			w_crop 	= float(w_crop_unscaled/img_width)
-			left_x 	= w_crop
-			right_x = 1.-w_crop
-			top_y	= 0.
-			bot_y	= 1.
-		else:
-			#height must be cropped
-			h_crop_unscaled = (img_height-img_width/aspect_ratio)/2
-			h_crop	= float(h_crop_unscaled/img_height)
-			left_x	= 0.
-			right_x	= 1.
-			top_y	= h_crop
-			bot_y	= 1.-h_crop
+			##get crop dimensions
+			if img_width > img_height*aspect_ratio:
+				#width must be cropped
+				w_crop_unscaled = (img_width-img_height*aspect_ratio)/2
+				w_crop 	= float(w_crop_unscaled/img_width)
+				left_x 	= w_crop
+				right_x = 1.-w_crop
+				top_y	= 0.
+				bot_y	= 1.
+			else:
+				#height must be cropped
+				h_crop_unscaled = (img_height-img_width/aspect_ratio)/2
+				h_crop	= float(h_crop_unscaled/img_height)
+				left_x	= 0.
+				right_x	= 1.
+				top_y	= h_crop
+				bot_y	= 1.-h_crop
 		
-		#crop image to aspect ratio
-		img.crop(left_x,top_y,right_x,bot_y)
-		logging.info(img)
-		#calculate output_height from output_width
-		output_height	= output_width/aspect_ratio
-		#resize cropped image
-		img.resize(width=int(output_width),height=int(output_height))
-		logging.info(img)
-		output_img = img.execute_transforms(output_encoding=images.JPEG)
-		logging.info(output_img)
-		self.response.out.write(output_img)
+			#crop image to aspect ratio
+			img.crop(left_x,top_y,right_x,bot_y)
+			logging.info(img)
+			#calculate output_height from output_width
+			output_height	= output_width/aspect_ratio
+			#resize cropped image
+			img.resize(width=int(output_width),height=int(output_height))
+			logging.info(img)
+			output_img = img.execute_transforms(output_encoding=images.JPEG)
+			logging.info(output_img)
+		except:
+			levr.log_error(self.request.body)
+			output_img = None
+		finally:
+			self.response.out.write(output_img)
 		
 app = webapp2.WSGIApplication([('/phone', phone),
 								('/phone/log', phone_log),

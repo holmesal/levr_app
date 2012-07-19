5ff7f6cb105f05d6d6a9c4306b57f4a07556c858
pnw <patrick@getlevr.com>
2012-07-19 12:27:22 -0400

    added stats viewer. see domain.com/_ah/stats

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,6 @@
 from gaesessions import SessionMiddleware 
+from google.appengine.ext.appstats import recording
 def webapp_add_wsgi_middleware(app): 
-	app = SessionMiddleware(app, cookie_key="dsijfb3459whefwiuhr39wfpwor209u30ru3wifjwj3f08wfuw08ef0384r38whf") 
+	app = SessionMiddleware(app, cookie_key="dsijfb3459whefwiuhr39wfpwor209u30ru3wifjwj3f08wfuw08ef0384r38whf")
+	app = recording.appstats_wsgi_middleware(app)
 	return app

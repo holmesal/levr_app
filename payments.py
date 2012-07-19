50711e9063ca993645722871ce7f5974ff74d81f
Alonso Holmes <holmesal@bu.edu>
2012-07-19 13:38:54 -0400

    Payments posting, hooking up COR functionality

--- payments.py
+++ payments.py
@@ -5,8 +5,12 @@ import levr_utils
 from google.appengine.ext import db
 import logging
 import jinja2
-import urllib2
+import urllib,urllib2
 from google.appengine.api import urlfetch
+
+#change this later
+import simplejson as json
+log = logging.getLogger(__name__)
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -14,67 +18,86 @@ def throwFlags(cor,ninja):
 	#check for a bunch of stuff - repeated payment requests, multiple requests from the same user, large dollar amounts, redemptions that happen too quickly, etc
 	pass
 
+class PaypalAdaptivePayment:
+	"""
+	Paypal Object to initialize and conducting the payments
+	"""
+	def __init__(self,paypal_sandbox_enabled):
+		'''Constructor for the Paypal Api Sets the headers and api credentials which are required for the initialization of the payments'''
+		assert paypal_sandbox_enabled, "missing arguments..."
+		self.request_data_format = 'JSON'
+		self.response_data_format = 'JSON'
+		self.paypal_sandbox_enabled = paypal_sandbox_enabled
+		if paypal_sandbox_enabled:
+			self.paypal_secure_user_id = "alonso_1342706801_biz_api1.getlevr.com"
+			self.paypal_secure_password = "1342706820"
+			self.paypal_api_signature = "Ahg-X6mZSS2MSKURJRVuRWFSIDuxAANmXWOAcd2gn4N1rF0KQmzwCfUt"
+			self.receiver_email = "alonso_1342706914_per@getlevr.com"
+			self.sender_email = "alonso_1342706801_biz@getlevr.com"
+			self.request_url =	"https://svcs.sandbox.paypal.com/AdaptivePayments/Pay"
+		else:
+			self.paypal_secure_user_id = "your live paypal secure user id"
+			self.paypal_secure_password = "your live secure password"
+			self.paypal_api_signature = "Your live ApI signature"
+			self.receiver_email = "Your Live Receiver Email"
+			self.request_url =	"https://paypal.com/AdaptivePayments/Pay"
+	
+	def initialize_payment(self,amount,cancel_url,return_url):
+		try:
+			header_data = {}
+			header_data["X-PAYPAL-SECURITY-USERID"] = self.paypal_secure_user_id
+			header_data["X-PAYPAL-SECURITY-PASSWORD"] = self.paypal_secure_password
+			header_data["X-PAYPAL-SECURITY-SIGNATURE"] = self.paypal_api_signature
+			header_data["X-PAYPAL-REQUEST-DATA-FORMAT"] = self.request_data_format
+			header_data["X-PAYPAL-RESPONSE-DATA-FORMAT"] = self.response_data_format
+			if self.paypal_sandbox_enabled:
+				header_data["X-PAYPAL-APPLICATION-ID"] = "APP-80W284485P519543T"
+			else:
+				header_data["X-PAYPAL-APPLICATION-ID"] = "Your Live Paypal Application ID"
+			params = {'actionType':'PAY', 'receiverList':{'receiver':[{'email':self.receiver_email,'amount':amount}]}, 'cancelUrl':cancel_url,'requestEnvelope':'', 'errorLanguage':'en_US', 'currencyCode':'USD', 'returnUrl':return_url,'senderEmail':self.sender_email}
+			paypal_request_data = json.dumps(params)
+			req = urllib2.Request(self.request_url,paypal_request_data,header_data)
+			response = urllib2.urlopen(req)
+			return json.loads(response.read())
+		except:
+			log.exception("Unable to initialize the payment flow...")
+	
+	
 class view(webapp2.RequestHandler):
 	def get(self):
 		cor = levr.CashOutRequest.gql('WHERE status=:1 ORDER BY amount DESC','pending').get()
-		ninja = levr.Customer.get(cor.key().parent())
-		
-		#count the number of deals from this person
-		q = levr.CustomerDeal.gql('WHERE ANCESTOR IS :1',ninja.key())
-		numDeals = q.count()
+		if cor:
+			ninja = levr.Customer.get(cor.key().parent())
 		
 		
-		template_values = {
-			"amount"					: cor.amount,
-			"life_paid"					: ninja.money_paid,
-			"numDeals"					: numDeals
-		}
+			#count the number of deals from this person
+			q = levr.CustomerDeal.gql('WHERE ANCESTOR IS :1',ninja.key())
+			numDeals = q.count()
+			
+			
+			template_values = {
+				"amount"					: cor.amount,
+				"life_paid"					: ninja.money_paid,
+				"numDeals"					: numDeals
+			}
+			
+			template = jinja_environment.get_template('templates/payments.html')
+			self.response.out.write(template.render(template_values))
 		
-		template = jinja_environment.get_template('templates/payments.html')
-		self.response.out.write(template.render(template_values))
+		else:
+			self.response.out.write('No cash-out-requests')
 
 class post(webapp2.RequestHandler):
 	def get(self):
-		'''
-		Currently a mockup for the payment functionality
-		Will accurately increment/decrement amounts available and such
-		just hook in actual payment service and go get a beer
-		'''
+
+		#Change to false and enter information in above class to deploy
+		paypal_init = PaypalAdaptivePayment(True)
+		response = paypal_init.initialize_payment("10","http://cancel_url.com","http://return_url.com")
 		
-		
-		#headers
-		headers = {
-			"X-PAYPAL-USERID"				: "alonso_1342706801_biz_api1.getlevr.com",
-			"X-PAYPAL-SECURITY-PASSWORD"	: "1342706820",
-			"X-PAYPAL-SECURITY-SIGNATURE"	: "Ahg-X6mZSS2MSKURJRVuRWFSIDuxAANmXWOAcd2gn4N1rF0KQmzwCfUt",
-			"X-PAYPAL-REQUEST-DATA-FORMAT"	: "JSON",
-			"X-PAYPAL-RESPONSE-DATA-FORMAT"	: "JSON",
-			"X-PAYPAL-APPLICATION-ID"		: "APP-80W284485P519543T"
-		}
-		
-		#headers = {'X-PAYPAL-USERID: alonso_1342706801_biz_api1.getlevr.com', 'X-PAYPAL-SECURITY-PASSWORD: 1342706820', 'X-PAYPAL-SECURITY-SIGNATURE: Ahg-X6mZSS2MSKURJRVuRWFSIDuxAANmXWOAcd2gn4N1rF0KQmzwCfUt', 'X-PAYPAL-REQUEST-DATA-FORMAT: JSON', 'X-PAYPAL-RESPONSE-DATA-FORMAT: JSON', 'X-PAYPAL-APPLICATION-ID: APP-80W284485P519543T'}
-		
-		#request body
-		request_body = {
-			"actionType"						: "PAY",
-			"senderEmail"						: "alonso_1342706801_biz_api1@getlevr.com",
-			"receiverList.receiver(0).email"	: "alonso_1342706914_per@getlevr.com",
-			"receiverList.receiver(0).amount"	: 100.00,
-			"currencyCode"						: "USD",
-			"cancelUrl"							: "getlevr.com",
-			"returnUrl"							: "getlevr.com",
-			"requestEnvelope.errorLanguage"		: "en_US"
-			
-		}
-		
-		url = "https://svcs.sandbox.paypal.com/AdaptivePayments/Pay"
-		
-		result = urlfetch.fetch(url=url,payload=request_body,headers=headers)
-		
-		logging.info(result.content)
+		self.response.out.write('<p>Payment status: <strong>'+ response['paymentExecStatus'] + '</strong></p><p><a href="/payments/view">Next Request</a></p>')
 		
 		#PAYPAL ACCOUNT INFORMATION
-		userid 			= None		#pretty sure this is the url from which the request is coming, and the url to which it will be returned
+		userid			= None		#pretty sure this is the url from which the request is coming, and the url to which it will be returned
 		password		= None
 		signature		= None
 		ipaddress		= None		#pretty sure this is the current IP address, NOT that of the phone. Makes sense, no?

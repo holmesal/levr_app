5318971e5aba2886c88714b4140e553cb350bb71
Alonso Holmes <holmesal@bu.edu>
2012-07-19 12:18:51 -0400

    added payment paypal api request to sandbox

--- payments.py
+++ payments.py
@@ -5,6 +5,8 @@ import levr_utils
 from google.appengine.ext import db
 import logging
 import jinja2
+import urllib2
+from google.appengine.api import urlfetch
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -32,12 +34,44 @@ class view(webapp2.RequestHandler):
 		self.response.out.write(template.render(template_values))
 
 class post(webapp2.RequestHandler):
-	def post(self):
+	def get(self):
 		'''
 		Currently a mockup for the payment functionality
 		Will accurately increment/decrement amounts available and such
 		just hook in actual payment service and go get a beer
 		'''
+		
+		
+		#headers
+		headers = {
+			"X-PAYPAL-USERID"				: "alonso_1342706801_biz_api1.getlevr.com",
+			"X-PAYPAL-SECURITY-PASSWORD"	: "1342706820",
+			"X-PAYPAL-SECURITY-SIGNATURE"	: "Ahg-X6mZSS2MSKURJRVuRWFSIDuxAANmXWOAcd2gn4N1rF0KQmzwCfUt",
+			"X-PAYPAL-REQUEST-DATA-FORMAT"	: "JSON",
+			"X-PAYPAL-RESPONSE-DATA-FORMAT"	: "JSON",
+			"X-PAYPAL-APPLICATION-ID"		: "APP-80W284485P519543T"
+		}
+		
+		#headers = {'X-PAYPAL-USERID: alonso_1342706801_biz_api1.getlevr.com', 'X-PAYPAL-SECURITY-PASSWORD: 1342706820', 'X-PAYPAL-SECURITY-SIGNATURE: Ahg-X6mZSS2MSKURJRVuRWFSIDuxAANmXWOAcd2gn4N1rF0KQmzwCfUt', 'X-PAYPAL-REQUEST-DATA-FORMAT: JSON', 'X-PAYPAL-RESPONSE-DATA-FORMAT: JSON', 'X-PAYPAL-APPLICATION-ID: APP-80W284485P519543T'}
+		
+		#request body
+		request_body = {
+			"actionType"						: "PAY",
+			"senderEmail"						: "alonso_1342706801_biz_api1@getlevr.com",
+			"receiverList.receiver(0).email"	: "alonso_1342706914_per@getlevr.com",
+			"receiverList.receiver(0).amount"	: 100.00,
+			"currencyCode"						: "USD",
+			"cancelUrl"							: "getlevr.com",
+			"returnUrl"							: "getlevr.com",
+			"requestEnvelope.errorLanguage"		: "en_US"
+			
+		}
+		
+		url = "https://svcs.sandbox.paypal.com/AdaptivePayments/Pay"
+		
+		result = urlfetch.fetch(url=url,payload=request_body,headers=headers)
+		
+		logging.info(result.content)
 		
 		#PAYPAL ACCOUNT INFORMATION
 		userid 			= None		#pretty sure this is the url from which the request is coming, and the url to which it will be returned

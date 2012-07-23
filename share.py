ddeeb277ae9664ccbb7f6f674038e6700b052d36
pnw <patrick@getlevr.com>
2012-07-23 12:59:29 -0400

    added exception handling to share.py

--- share.py
+++ share.py
@@ -13,150 +13,153 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 class share(webapp2.RequestHandler):
 	def get(self):
-
-		#grab refKey
-		dealID = enc.decrypt_key(self.request.get('id'))
-		logging.info(dealID)
-		error = self.request.get('error')
-		success = self.request.get('success')
-		
-		#propogate user-entered values
-		email_or_owner = self.request.get('email_or_owner')
-		email = self.request.get('email')
-		username = self.request.get('username')
-		
-		#grab deal from datastore
 		try:
-			deal = levr.Deal.get(dealID)
-		except:
-			logging.error('Could not grab deal. id passed: ' + dealID)
-			sys.exit()
-			
-		if deal:
-			#check loginstate
-			headerData = levr_utils.loginCheck(self,False)
-			
-			#get alias from parent (ninja)
-			ninja = levr.Customer.get(deal.key().parent())
-			alias = ninja.alias
-			
-			#format deal
-			dealFormatted = levr.phoneFormat(deal,'list')
-			
-			template_values = {
-				'headerData' 	: headerData,
-				'title' 		: 'Share',
-				'deal'			: deal,
-				'dealID'		: enc.encrypt_key(dealID),
-				'alias'			: alias,
-				'dealText'		: dealFormatted['dealText'],
-				'dealTextExtra'	: dealFormatted['dealTextExtra'],
-				'description'	: deal.description,
-				'error'			: error,
-				'success'		: success,
-				'email_or_owner': email_or_owner,
-				'email'			: email,
-				'username'		: username
-			}
-			
-			#jinja2
-			template = jinja_environment.get_template('templates/share.html')
-			self.response.out.write(template.render(template_values))
-
-			
-			
-			
-		else:
-			self.redirect('/error')
-		logging.info(deal.__dict__)
+			#grab refKey
+			dealID = enc.decrypt_key(self.request.get('id'))
+			logging.info(dealID)
+			error = self.request.get('error')
+			success = self.request.get('success')
 		
+			#propogate user-entered values
+			email_or_owner = self.request.get('email_or_owner')
+			email = self.request.get('email')
+			username = self.request.get('username')
+		
+			#grab deal from datastore
+			try:
+				deal = levr.Deal.get(dealID)
+			except:
+				logging.error('Could not grab deal. id passed: ' + dealID)
+				sys.exit()
+			
+			if deal:
+				#check loginstate
+				headerData = levr_utils.loginCheck(self,False)
+			
+				#get alias from parent (ninja)
+				ninja = levr.Customer.get(deal.key().parent())
+				alias = ninja.alias
+			
+				#format deal
+				dealFormatted = levr.phoneFormat(deal,'list')
+			
+				template_values = {
+					'headerData' 	: headerData,
+					'title' 		: 'Share',
+					'deal'			: deal,
+					'dealID'		: enc.encrypt_key(dealID),
+					'alias'			: alias,
+					'dealText'		: dealFormatted['dealText'],
+					'dealTextExtra'	: dealFormatted['dealTextExtra'],
+					'description'	: deal.description,
+					'error'			: error,
+					'success'		: success,
+					'email_or_owner': email_or_owner,
+					'email'			: email,
+					'username'		: username
+				}
+			
+				#jinja2
+				template = jinja_environment.get_template('templates/share.html')
+				self.response.out.write(template.render(template_values))
+			else:
+				self.redirect('/error')
+			logging.info(deal.__dict__)
+		except:
+			levr.log_error()
 		
 class loginFav(webapp2.RequestHandler):
 	def post(self):
-		#login, then add
-		email_or_owner = self.request.get('email_or_owner')
-		pw = self.request.get('pw')
+		try:
+			#login, then add
+			email_or_owner = self.request.get('email_or_owner')
+			pw = self.request.get('pw')
 		
-		session = get_current_session()
-		
-		dealID = enc.decrypt_key(self.request.get('id'))
-		
-		#attempt login
-		response = levr_utils.loginCustomer(email_or_owner,pw)
-		logging.info(response)
-		if response['success'] == True:
-			#grab customer
-			customer = levr.Customer.get(enc.decrypt_key(response['uid']))
-			#if matched, pull properties and set loginstate to true
-			session['uid'] = enc.encrypt_key(customer.key())
-			session['alias'] = customer.alias
-			session['loggedIn'] = True
-			#add deal to fav
-			fav = levr.Favorite(parent=customer.key())
-			fav.dealID = dealID
-			fav.put()
-			#redirect to success page
-			self.redirect('/share/deal?id=' + dealID + '&success=1')
-		else:
-			dealID = enc.encrypt_key(dealID)
-			#show login page again, with login error
-			self.redirect('/share/deal?id='+dealID+'&error=login&email_or_owner='+email_or_owner)
-			
-	
-class signupFav(webapp2.RequestHandler):
-	def post(self):
-		#pull signup info from post
-		email = self.request.get('email')
-		alias = self.request.get('alias')
-		pw = self.request.get('pw')
-		
-		dealID = enc.decrypt_key(self.request.get('id'))
-		
-		logging.info(len(pw))
-		logging.info(type(len(pw)))
-		
-		if len(pw) < 6:
-			dealID = enc.encrypt_key(dealID)
-			self.redirect('/share/deal?id='+dealID+'&error=password'+'&email='+email+'&username='+alias)
-		else:
-
 			session = get_current_session()
-			
-			response = levr_utils.signupCustomer(email,alias,pw)
-			
-			if response['success']:
-				#add to favorites
-				customer = levr.Customer.get(response['uid'])
-				fav = levr.Favorite(parent=customer.key())
-				fav.dealID = dealID
-				fav.put()
-				#login user
+		
+			dealID = enc.decrypt_key(self.request.get('id'))
+		
+			#attempt login
+			response = levr_utils.loginCustomer(email_or_owner,pw)
+			logging.info(response)
+			if response['success'] == True:
+				#grab customer
+				customer = levr.Customer.get(enc.decrypt_key(response['uid']))
+				#if matched, pull properties and set loginstate to true
 				session['uid'] = enc.encrypt_key(customer.key())
 				session['alias'] = customer.alias
 				session['loggedIn'] = True
-				dealID = enc.encrypt_key(dealID)
+				#add deal to fav
+				fav = levr.Favorite(parent=customer.key())
+				fav.dealID = dealID
+				fav.put()
+				#redirect to success page
 				self.redirect('/share/deal?id=' + dealID + '&success=1')
 			else:
 				dealID = enc.encrypt_key(dealID)
-				self.redirect('/share/deal?id='+dealID+'&error='+response['field']+'&email='+email+'&username='+alias)
+				#show login page again, with login error
+				self.redirect('/share/deal?id='+dealID+'&error=login&email_or_owner='+email_or_owner)
+		except:
+			levr.log_error()
+	
+class signupFav(webapp2.RequestHandler):
+	def post(self):
+		try:
+			#pull signup info from post
+			email = self.request.get('email')
+			alias = self.request.get('alias')
+			pw = self.request.get('pw')
+		
+			dealID = enc.decrypt_key(self.request.get('id'))
+		
+			logging.info(len(pw))
+			logging.info(type(len(pw)))
+		
+			if len(pw) < 6:
+				dealID = enc.encrypt_key(dealID)
+				self.redirect('/share/deal?id='+dealID+'&error=password'+'&email='+email+'&username='+alias)
+			else:
 
+				session = get_current_session()
+			
+				response = levr_utils.signupCustomer(email,alias,pw)
+			
+				if response['success']:
+					#add to favorites
+					customer = levr.Customer.get(response['uid'])
+					fav = levr.Favorite(parent=customer.key())
+					fav.dealID = dealID
+					fav.put()
+					#login user
+					session['uid'] = enc.encrypt_key(customer.key())
+					session['alias'] = customer.alias
+					session['loggedIn'] = True
+					dealID = enc.encrypt_key(dealID)
+					self.redirect('/share/deal?id=' + dealID + '&success=1')
+				else:
+					dealID = enc.encrypt_key(dealID)
+					self.redirect('/share/deal?id='+dealID+'&error='+response['field']+'&email='+email+'&username='+alias)
+		except:
+			levr.log_error()
 class loggedInFav(webapp2.RequestHandler):
 	def get(self):
-		dealID = enc.decrypt_key(self.request.get('id'))
+		try:
+			dealID = enc.decrypt_key(self.request.get('id'))
 		
-		session = get_current_session()
+			session = get_current_session()
 		
-		customerKey = enc.decrypt_key(session['uid'])
+			customerKey = enc.decrypt_key(session['uid'])
 		
-		customer = levr.Customer.get(customerKey)
+			customer = levr.Customer.get(customerKey)
 		
-		fav = levr.Favorite(parent=customer.key())
-		fav.dealID = dealID
-		fav.put()
+			fav = levr.Favorite(parent=customer.key())
+			fav.dealID = dealID
+			fav.put()
 		
-		dealID = enc.encrypt_key(dealID)
-		self.redirect('/share/deal?id=' + dealID + '&success=1')
-	
+			dealID = enc.encrypt_key(dealID)
+			self.redirect('/share/deal?id=' + dealID + '&success=1')
+		except:
+			levr.log_error()
 class success(webapp2.RequestHandler):
 	def get(self):
 		print('success!')

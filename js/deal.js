93cbfd5bee71918fac7661055db5ae230a703861
Alonso Holmes <holmesal@bu.edu>
2012-07-27 20:23:06 -0400

    Busy breaking autocomplete tests

--- js/deal.js
+++ js/deal.js
@@ -1,31 +1,44 @@
 $(document).ready(function() {
 	
+	//register autocomplete
+	var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		
+	$('#business_select').autocomplete({
+		source: availableTags
+	});
+	
 	//register typing listeners
 	$('.textin').keyup(function() {
 		$('#'+this.name).text($(this).val())
 	})
 	
+	
 	$('#business_select').keyup(function() {
 		url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + $(this).val() + '&sensor=false&types=establishment&key=AIzaSyDA6oSrrTbFLHVqZSt2JGeJWFLVa9F0074';
 		console.log(url)
-		
-		$.ajax({
-			url: 'http://api.hostip.info/get_json.php',
-			type: 'GET',
-			crossdomain: true,
-			success: function(result) {
-				console.log(result)
-				console.log('success')
-				result['Content-Length'] = len(result)
-			}
-			
-		});
-		
-		/*$.get('http://api.hostip.info/get_json.php', function(data) {
-		  $('.result').html(data);
-		  alert('Load was performed.');
-		});*/
-		
 		
 		$.ajax({
 			url: url,
@@ -35,5 +48,6 @@ $(document).ready(function() {
 			}
 			
 		});
+		
 	});
 });

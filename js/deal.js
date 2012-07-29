711ece2037674e267c2e925f195bc8fda8716ffb
Alonso Holmes <holmesal@bu.edu>
2012-07-29 15:24:50 -0400

    cleaned up lots of merchants code

--- js/deal.js
+++ js/deal.js
@@ -1,47 +1,8 @@
-function autocomplete_click() {
-	$('.result').click(function(){
-		//populate box and phone
-		$('#business_select').val($('.result_primary',this).text())
-		$('#deal_business').text($('.result_primary',this).text())
-		//hide autocomplete
-		$('#results').css('visibility','hidden')
-		//go get places detail request, reference is this.id
-		url = 'https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyDA6oSrrTbFLHVqZSt2JGeJWFLVa9F0074&sensor=false&reference='+this.id
-		$.ajax({
-				url: url,
-				type: 'GET',
-				success: function(result) {
-					$('#deal_address').text(result.result.vicinity);
-				}
-				
-			});
-	})
-	
-}
-
-function populate_autocomplete(predictions) {
-	//empty current autocomplete
-	$('#results').empty();
-	
-	$.each(predictions, function(i,prediction){
-		primary = prediction.terms[0].value
-		//split string after comma
-		idx = prediction.description.indexOf(',') + 2
-		secondary = prediction.description.substring(idx)
-		
-		html_string = '<div class="result" id="'+prediction.reference+'"><p class="result_primary">'+primary+'</p><p class="result_secondary">'+secondary+'</p></div>';
-		
-		//append new result
-		$('#results').append(html_string);
-	})
-	//register listeners
-	autocomplete_click();	
-}
-
 $(document).ready(function() {
 	
 	//register typing listeners
-	$('.textin').keyup(function() {
+	$('.textIn').keyup(function() {
+		console.log(this)
 		//if it's deal line2, add inputted text to the middle of a parenthetical
 		if (this.name=='deal_line2' && $(this).val().length > 0) {
 			$('#deal_line2').text('(with purchase of '+$(this).val()+')')
@@ -52,30 +13,18 @@ $(document).ready(function() {
 		}
 	})
 	
-	//typing listeners for business selector
-	$('#business_select').keyup(function() {
-		if ($(this).val().length > 2){
-			url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + $(this).val() + '&sensor=false&types=establishment&key=AIzaSyDA6oSrrTbFLHVqZSt2JGeJWFLVa9F007';
-			
-			$.ajax({
-				url: url,
-				type: 'GET',
-				success: function(result) {
-					$('#results').css('visibility','visible')
-					populate_autocomplete(result.predictions)
-				}
-				
-			});
-		} else{
-			$('#results').css('visibility','hidden')
-		}
-	});
+	//initialize places service
+	var input = document.getElementById('business_select');
+	var options = {types: ['establishment']};
+	autocomplete = new google.maps.places.Autocomplete(input, options);
 	
-	/* This is broken - it triggers the click event, but after the file dialog closes the change event is not triggered
-	//click listener for image upload pretty button
-	$('#uploaded_img').click(function() {
-		$('#img_upload').click();
-	})*/
+	//initialize place_changed listener
+	var place = {};
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		var place = autocomplete.getPlace();
+		$('#deal_business').text(place.name);
+		$('#deal_address').text(place.vicinity);
+	});
 	
 	//listen for a change in the file button, use that to upload images
 	$('#img_upload').change(function(){

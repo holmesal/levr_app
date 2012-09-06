4fc0aa7d3552804f18a75dfe926d26bfbd99648d
pnw <patrick@getlevr.com>
2012-09-06 13:52:58 -0400

    targeted businesses script ready for upload

--- store_businesses.py
+++ store_businesses.py
@@ -12,32 +12,31 @@ class StoreBusinessesHandler(webapp2.RequestHandler):
 		#split into business entries
 		businesses_list	= conglomerate.split('\n')
 		
-		#remove the last line which is empty
-		businesses_list.pop()
 		
 		logging.debug(businesses_list)
 		#perform on each business entry
 		for b in businesses_list:
-			#split entry
-			business	= b.split('\t')
-			logging.debug(business)
-			#grab data from entry
-			name		= business[0]
-			vicinity	= business[1]
-			geo_point	= levr.geo_converter(business[2])
-			types		= business[3].split(',')
-			
-			#store as entity
-			b	= levr.Business()
-			b.business_name	= name
-			b.vicinity		= vicinity
-			b.geo_point		= geo_point
-			b.types			= types
-			b.targeted		= True
-			b.put()
-			
-			#confirm storage
-			self.response.out.write(str(b.key())+"<br />")
+			if b != []:
+				#split entry
+				business	= b.split('\t')
+				logging.debug(business)
+				#grab data from entry
+				name		= business[0]
+				vicinity	= business[1]
+				geo_point	= levr.geo_converter(business[2])
+				types		= business[3].split(',')
+				
+				#store as entity
+				b	= levr.Business()
+				b.business_name	= name
+				b.vicinity		= vicinity
+				b.geo_point		= geo_point
+				b.types			= types
+				b.targeted		= True
+				b.put()
+				
+				#confirm storage
+				self.response.out.write(name+" "+vicinity+"<br />")
 
 app = webapp2.WSGIApplication([('/target', StoreBusinessesHandler)
 								], debug=True)

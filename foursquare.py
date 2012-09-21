426d0659b1c5b63758b5e99ccabfe57c8ba91afb
Alonso Holmes <holmesal@bu.edu>
2012-09-21 17:48:09 -0400

    spoofing business for now

--- foursquare.py
+++ foursquare.py
@@ -8,6 +8,7 @@ import levr_utils
 import logging
 import jinja2
 from google.appengine.api import urlfetch
+import urllib
 import json
 
 #CASES:
@@ -31,6 +32,7 @@ class AuthorizeHandler(webapp2.RequestHandler):
 class PushHandler(webapp2.RequestHandler):
 	def post(self):
 		logging.debug('Foursquare push request received!')
+		logging.debug(self.request.body)
 		checkin = json.loads(self.request.get('checkin'))
 		secret = self.request.get('secret')
 		logging.debug(levr_utils.log_dict(checkin))
@@ -41,15 +43,16 @@ class PushHandler(webapp2.RequestHandler):
 			#raise an exception
 			logging.debug('SECRETS DO NOT MATCH')
 		
-		'''#go look in our database for a matching foursquare venue id
-		business = levr.Business.gql('WHERE foursquare_id IS :1',checkin.venue.id).get()
+		#go look in our database for a matching foursquare venue id
+		#business = levr.Business.gql('WHERE foursquare_id IS :1',checkin.venue.id).get()
+		business = levr.Business.get('ahFzfmxldnItcHJvZHVjdGlvbnIQCxIIQnVzaW5lc3MY-dIBDA')
 		
 		#initialize the response object
 		reply = {
-			CHECKIN_ID		: checkin.id,
-			text			: '',
-			url				: '',
-			contentID		: ''
+			'CHECKIN_ID'		: 'id',
+			'text'				: 'hi there!',
+			'url'				: 'http://www.levr.com',
+			'contentID'			: 'BWANHHPAHAHA'
 		}
 		
 		if business:	#business found
@@ -58,34 +61,35 @@ class PushHandler(webapp2.RequestHandler):
 			numdeals = q.count()
 			if numdeals > 1:	#many deals found
 				topdeal = q.get()
-				reply.text = "There are deals here! "+topdeal.deal_text + ". Click to see all "+numdeals+" deals."
-				reply.url = '' #deeplink into dealResults screen
+				reply['text'] = "There are deals here! "+topdeal.deal_text + ". Click to see all "+numdeals+" deals."
+				reply['url'] = '' #deeplink into dealResults screen
 			elif numdeals == 1:	#only one deal found
 				topdeal = q.get()
-				reply.text = "There's a deal here! "+topdeal.deal_text+". Click to redeem."
-				reply.url = '' #deeplink into dealDetail screen
+				reply['text'] = "There's a deal here! "+topdeal.deal_text+". Click to redeem."
+				reply['url'] = '' #deeplink into dealDetail screen
 			else:	#no deals found
-				reply.text = "There aren't any deals here - maybe you'll be the first to add one? Click to upload."
-				reply.url = '' #deeplink into deal upload screen
+				reply['text'] = "There aren't any deals here - maybe you'll be the first to add one? Click to upload."
+				reply['url'] = '' #deeplink into deal upload screen
 		else:			#no business found
-			reply.text = "There aren't any deals here - maybe you'll be the first to add one? Click to upload."
-			reply.url = '' #deeplink into deal upload screen
+			reply['text'] = "There aren't any deals here - maybe you'll be the first to add one? Click to upload."
+			reply['url'] = '' #deeplink into deal upload screen
 			
-		'''
 		
-		reply = {
+		'''reply = {
 			'CHECKIN_ID'		: checkin['id'],
 			'text'				: 'hi there!',
 			'url'				: 'http://www.levr.com',
-			'contentID'			: ''
-		}
+			'contentID'			: 'BWANHHPAHAHA'
+		}'''
 			
-		url = 'https://api.foursquare.com/v2/checkins/'+reply['CHECKIN_ID']+'/reply'
+		url = 'https://api.foursquare.com/v2/checkins/'+reply['CHECKIN_ID']+'/reply?v=20120920&oauth_token='+'PZVIKS4EH5IFBJX1GH5TUFYAA3Z5EX55QBJOE3YDXKNVYESZ'
 		logging.debug(url)
+		data = urllib.urlencode(reply)
+		logging.debug(data)
 		result = urlfetch.fetch(url=url,
-								payload=json.dumps(reply),
+								payload=data,
 								method=urlfetch.POST)
-		logging.debug(result)
+		logging.debug(levr_utils.log_dict(result.__dict__))
 		
 		
 app = webapp2.WSGIApplication([('/foursquare/push', PushHandler),
